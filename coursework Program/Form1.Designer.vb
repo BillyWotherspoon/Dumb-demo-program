2d173da3165eedd1d72f3fca7eab51492ae6a70e
Billy Wotherspoon <billy.wotherspoon@egglighting.com>
2022-03-16 09:31:56 +0000

    New changes

--- coursework Program/Form1.Designer.vb
+++ coursework Program/Form1.Designer.vb
@@ -33,6 +33,11 @@ Partial Class Form1
         Me.Label2 = New System.Windows.Forms.Label()
         Me.TextBox3 = New System.Windows.Forms.TextBox()
         Me.Label3 = New System.Windows.Forms.Label()
+        Me.exponentiate = New System.Windows.Forms.Button()
+        Me.Modulus = New System.Windows.Forms.Button()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Button3 = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'Button1
@@ -46,11 +51,11 @@ Partial Class Form1
         '
         'Button2
         '
-        Me.Button2.Location = New System.Drawing.Point(12, 208)
+        Me.Button2.Location = New System.Drawing.Point(13, 258)
         Me.Button2.Name = "Button2"
-        Me.Button2.Size = New System.Drawing.Size(298, 44)
+        Me.Button2.Size = New System.Drawing.Size(140, 44)
         Me.Button2.TabIndex = 0
-        Me.Button2.Text = "Clear"
+        Me.Button2.Text = "Clear current"
         Me.Button2.UseVisualStyleBackColor = True
         '
         'TextBox1
@@ -129,18 +134,68 @@ Partial Class Form1
         Me.Label3.TabIndex = 2
         Me.Label3.Text = "Result"
         '
+        'exponentiate
+        '
+        Me.exponentiate.Location = New System.Drawing.Point(12, 208)
+        Me.exponentiate.Name = "exponentiate"
+        Me.exponentiate.Size = New System.Drawing.Size(140, 44)
+        Me.exponentiate.TabIndex = 0
+        Me.exponentiate.Text = "Exponetiation"
+        Me.exponentiate.UseVisualStyleBackColor = True
+        '
+        'Modulus
+        '
+        Me.Modulus.Location = New System.Drawing.Point(159, 208)
+        Me.Modulus.Name = "Modulus"
+        Me.Modulus.Size = New System.Drawing.Size(151, 44)
+        Me.Modulus.TabIndex = 0
+        Me.Modulus.Text = "Modulus"
+        Me.Modulus.UseVisualStyleBackColor = True
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 15
+        Me.ListBox1.Location = New System.Drawing.Point(332, 48)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(185, 244)
+        Me.ListBox1.TabIndex = 3
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(332, 30)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(92, 15)
+        Me.Label4.TabIndex = 2
+        Me.Label4.Text = "Previous Results"
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(170, 258)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(140, 44)
+        Me.Button3.TabIndex = 0
+        Me.Button3.Text = "Clear Previous"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(7.0!, 15.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(318, 262)
+        Me.ClientSize = New System.Drawing.Size(529, 307)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Controls.Add(Me.Label4)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.Label1)
         Me.Controls.Add(Me.TextBox3)
         Me.Controls.Add(Me.TextBox2)
         Me.Controls.Add(Me.TextBox1)
+        Me.Controls.Add(Me.Button3)
         Me.Controls.Add(Me.Button2)
+        Me.Controls.Add(Me.exponentiate)
+        Me.Controls.Add(Me.Modulus)
         Me.Controls.Add(Me.Devide)
         Me.Controls.Add(Me.Multiply)
         Me.Controls.Add(Me.Subtract)
@@ -163,4 +218,9 @@ Partial Class Form1
     Friend WithEvents Label2 As Label
     Friend WithEvents TextBox3 As TextBox
     Friend WithEvents Label3 As Label
+    Friend WithEvents exponentiate As Button
+    Friend WithEvents Modulus As Button
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Button3 As Button
 End Class
--- coursework Program/Form1.vb
+++ coursework Program/Form1.vb
@@ -1,66 +1,107 @@
 ﻿Public Class Form1
+
+    'Simple Calculator with advanced functions  
+    'created on 16/03/2022
     Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+
+        'clear all Values from boxes
         TextBox1.Clear()
         TextBox2.Clear()
         TextBox3.Clear()
-
-
     End Sub
 
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
 
-        Dim num1 As Integer
-        Dim num2 As Integer
-        Dim sum As Integer
+        'declare variables
 
+        Dim num1 As Single
+        Dim num2 As Single
+        Dim sum As Single
+
+        'get user input
         num1 = TextBox1.Text
         num2 = TextBox2.Text
+
+        'Perform operation
         sum = num1 + num2
 
-        MessageBox.Show(sum)
-
+        TextBox3.Text = sum
+        ListBox1.Items.Add(sum)
 
     End Sub
 
     Private Sub Subtract_Click(sender As Object, e As EventArgs) Handles Subtract.Click
 
-        Dim num1 As Integer
-        Dim num2 As Integer
-        Dim sum As Integer
+        Dim num1 As Single
+        Dim num2 As Single
+        Dim sum As Single
 
         num1 = TextBox1.Text
         num2 = TextBox2.Text
         sum = num1 - num2
-
-        MessageBox.Show(sum)
-
+        ListBox1.Items.Add(sum)
+        TextBox3.Text = sum
     End Sub
 
     Private Sub Devide_Click(sender As Object, e As EventArgs) Handles Devide.Click
 
-        Dim num1 As Integer
-        Dim num2 As Integer
-        Dim sum As Integer
+        Dim num1 As Single
+        Dim num2 As Single
+        Dim sum As Single
 
         num1 = TextBox1.Text
         num2 = TextBox2.Text
         sum = num1 / num2
 
-        MessageBox.Show(sum)
-
+        TextBox3.Text = sum
+        ListBox1.Items.Add(sum)
     End Sub
 
     Private Sub Multiply_Click(sender As Object, e As EventArgs) Handles Multiply.Click
 
-        Dim num1 As Integer
-        Dim num2 As Integer
-        Dim sum As Integer
+        Dim num1 As Single
+        Dim num2 As Single
+        Dim sum As Single
 
         num1 = TextBox1.Text
         num2 = TextBox2.Text
         sum = num1 * num2
 
-        MessageBox.Show(sum)
+        TextBox3.Text = sum
+        ListBox1.Items.Add(sum)
+    End Sub
 
+    Private Sub exponentiate_Click(sender As Object, e As EventArgs) Handles exponentiate.Click
+        Dim num1 As Single
+        Dim num2 As Single
+        Dim sum As Single
+
+
+        num1 = TextBox1.Text
+        num2 = TextBox2.Text
+        sum = num1 ^ num2
+
+        TextBox3.Text = sum
+        ListBox1.Items.Add(sum)
+    End Sub
+
+    Private Sub Modulus_Click(sender As Object, e As EventArgs) Handles Modulus.Click
+
+        Dim num1 As Single
+        Dim num2 As Single
+        Dim sum As Single
+
+
+        num1 = TextBox1.Text
+        num2 = TextBox2.Text
+        sum = num1 Mod num2
+
+        TextBox3.Text = sum
+        ListBox1.Items.Add(sum)
+
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        ListBox1.Items.Clear()
     End Sub
 End Class
